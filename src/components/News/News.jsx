6b6df52dc7a4525eaba0efbfb055c0280d75b62a
Libor <libor.gess11@gmail.com>
2025-01-29 20:47:07 +0100

    fix layout

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,31 +1,40 @@
+const localNewsData = [
+    {
+      title: "French people enjoy",
+      content: "The French people enjoy"
+    },
+    {
+      title: "Italy has new rules",
+      content: "Italy has new rules ..."
+    },
+    {
+      title: "A new Study showed",
+      content: "The fox has a genetic ..."
+    },
+    {
+      title: "The Polish Government",
+      content: "Wants to spend extra money ..."
+    }
+  ];
+
 import localNews from './localNews.module.css';
+import img from '../../assets/snake.png';
 
 const News = () => {
     return (
         <div className={localNews.cardContainer}>
-            <div className={localNews.card}>
-                <div className={localNews.title}>French people enjoy</div>
-                <p>The French people enjoy</p>
+          {localNewsData.map((n, index) =>
+            <div key={index} className={localNews.card}>
+                      <div className={localNews.gradient}></div>
+                      <img src={img} alt="" />
+              <div className={localNews.details}>
+      
+                <div className={localNews.title}>{n.title}</div>
+                <p>{n.content}</p>
+              </div>
             </div>
-
-            <div className={localNews.card}>
-                <div className={localNews.title}>Italy has new rulles</div>
-                <p>Italy has new rulles ...</p>
-            </div>
-
-            <div className={localNews.card}>
-                <div className={localNews.title}>A new Study showed</div>
-                <p>The fox has a genetic ...</p>
-            </div>
-
-            <div className={localNews.card}>
-                <div className={localNews.title}>A new Study showed</div>
-                <p>The fox has a genetic ...</p>
-            </div>
-
+          )}
         </div>
-
-        
     )
 }
 
